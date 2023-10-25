1857cce3966f08d6b663636769e525856e95ef7d
ShovonPaul <110727330+ShovonPaul@users.noreply.github.com>
2023-10-25 17:24:14 +0530

    Add files via upload

---
+++ project-C109-template-main/take_screenshot.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pyautogui
+import imutils
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+cap = cv2.VideoCapture(0)
+
+finger_tips = [8, 12, 16, 20]
+thumb_tip = 4
+
+while True:
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    h, w, c = img.shape
+    results = hands.process(img)
+
+    if results.multi_hand_landmarks:
+        for hand_landmark in results.multi_hand_landmarks:
+            lm_list = []
+            for id, lm in enumerate(hand_landmark.landmark):
+                lm_list.append(lm)
+
+            finger_fold_status = []
+            for tip in finger_tips:
+                x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
+                cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
+
+                if lm_list[tip].x < lm_list[tip - 3].x:
+                    cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
+                    finger_fold_status.append(True)
+                else:
+                    finger_fold_status.append(False)
+
+            print(finger_fold_status)
+
+            if all(finger_fold_status):
+                # Taking screenshot
+                screenshot = pyautogui.screenshot()
+                screenshot_np = np.array(screenshot)
+                screenshot_cv2 = cv2.cvtColor(screenshot_np, cv2.COLOR_RGB2BGR)
+
+                # Save screenshot to disk
+                cv2.imwrite("screenshot.png", screenshot_cv2)
+
+                # Load image in OpenCV format
+                loaded_img = cv2.imread("screenshot.png")
+                # Use the loaded_img variable as needed
+
+            mp_draw.draw_landmarks(img, hand_landmark,
+                                    mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0, 0, 255), 2, 2),
+                                    mp_draw.DrawingSpec((0, 255, 0), 4, 2))
+
+    cv2.imshow("hand tracking", img)
+    cv2.waitKey(1)
